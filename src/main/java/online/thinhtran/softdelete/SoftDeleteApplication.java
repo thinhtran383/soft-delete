17e6c3f33a73a665911b2b39621151c85c7d82a6
ThinhTran <thinhtran3803@gmail.com>
2025-03-17 00:43:06 +0700

    init

---
+++ src/main/java/online/thinhtran/softdelete/SoftDeleteApplication.java
@@ -0,0 +1,13 @@
+package online.thinhtran.softdelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftDeleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftDeleteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/thinhtran/softdelete/SoftDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package online.thinhtran.softdelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftDeleteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
